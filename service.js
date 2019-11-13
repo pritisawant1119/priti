6b3693880bac9ad9d2f2ae0d2ad8f4b169ae9fd5
priti sawant <sawantpriti19@gmail.com>
2019-11-13 15:23:34 +0530

    initial commit

---
+++ service.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app= express();
+
+app.get('/', (request, response) => {
+    response.send('welcome to my application')
+})
+app.post('/', (request, response) => {
+    response.send('welcome to my application')
+})
+
+
+app.listen(4000, () => {
+    console.log(`Server started on port 4000`);
+});
